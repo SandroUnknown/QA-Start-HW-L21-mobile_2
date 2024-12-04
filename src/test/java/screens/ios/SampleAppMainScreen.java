0fb3e6ff7ede34a4ae0244a0a2df70c20164bd46
Alexander Petyukov <sandrounknown1991@gmail.com>
2024-12-04 19:09:35 +0300

    init commit

--- src/test/java/screens/ios/SampleAppMainScreen.java
+++ src/test/java/screens/ios/SampleAppMainScreen.java
@@ -1,29 +1,17 @@
 package screens.ios;
 
-import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Selenide.$;
-import static io.appium.java_client.AppiumBy.accessibilityId;
 import static io.appium.java_client.AppiumBy.name;
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class SampleAppMainScreen {
-
-    //private final SelenideElement buttonElement = $(accessibilityId("Text Button"));
-    //private final SelenideElement inputElement = $(accessibilityId("Text Input"));
-    //private final SelenideElement outputElement = $(accessibilityId("Text Output"));
 
     @Step("Проверить наличие надписи")
     public SampleAppMainScreen checkExistText(String text) {
 
-        //$(name(text)).shouldBe(exist);
-
-        SelenideElement outputElement = $(name(text));
-        assertThat(outputElement.isDisplayed()).isTrue();
-
-
+        $(name(text)).shouldBe(exist);
         return this;
     }
 }
--- src/test/java/tests/ios/IosTests.java
+++ src/test/java/tests/ios/IosTests.java
@@ -1,5 +1,6 @@
 package tests.ios;
 
+import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
@@ -8,16 +9,15 @@ import screens.ios.SampleAppMainScreen;
 import screens.ios.SampleAppSearchScreen;
 import tests.TestBase;
 
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.$;
-import static io.appium.java_client.AppiumBy.name;
-
 @Tag("ios")
 @Owner("Petyukov Alexander")
-@DisplayName("Проверки для Android")
+@DisplayName("Проверки для iOS")
 public class IosTests extends TestBase {
 
-    //@Test
+    @Test
+    @Feature("Страница поиска")
+    @Feature("Позитивная проверка")
+    @DisplayName("Проверить, что 'поиск' работает")
     public void successfulSearchTest() {
 
         String query = "Appium";
@@ -32,12 +32,15 @@ public class IosTests extends TestBase {
     }
 
     @Test
-    public void visibleMainPageTest() {
+    @Feature("Главная страница")
+    @Feature("Позитивная проверка")
+    @DisplayName("Проверить, что поиск статей работает")
+    public void successfulCheckExistTextTest() {
 
         SampleAppMainScreen mainScreen = new SampleAppMainScreen();
 
         mainScreen
-                .checkExistText("Text1")
+                .checkExistText("Text")
                 .checkExistText("Alert");
     }
 }
