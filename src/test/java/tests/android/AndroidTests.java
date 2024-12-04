0f6775054969e39591c876af730c8a3c554cc5af
Alexander Petyukov <sandrounknown1991@gmail.com>
2024-12-04 16:24:47 +0300

    init commit

--- src/test/java/tests/android/AndroidTests.java
+++ src/test/java/tests/android/AndroidTests.java
@@ -1,13 +1,19 @@
-package tests;
+package tests.android;
 
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import screens.WikipediaErrorScreen;
 import screens.WikipediaMainScreen;
+import tests.TestBase;
 
-public class SearchTests extends TestBase {
-
+@Tag("android")
+@Owner("Petyukov Alexander")
+public class AndroidTests extends TestBase {
 
     @Test
+    @DisplayName("Проверить, что поиск статей работает")
     void successfulSearchTest() {
 
         WikipediaMainScreen mainPage = new WikipediaMainScreen();
@@ -20,6 +26,7 @@ public class SearchTests extends TestBase {
     }
 
     @Test
+    @DisplayName("Проверить, что открытие статьи из результата поиска НЕ работает (негативный тест)")
     void unsuccessfulSearchTest() {
 
         WikipediaMainScreen mainPage = new WikipediaMainScreen();
