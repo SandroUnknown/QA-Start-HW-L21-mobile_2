cbc72c0ba1e889fff1eb4bf1ec94e3ae3cde1707
Alexander Petyukov <sandrounknown1991@gmail.com>
2024-12-04 12:56:01 +0300

    init commit

--- src/test/java/tests/SearchTests.java
+++ src/test/java/tests/SearchTests.java
@@ -3,6 +3,7 @@ package tests;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static groovy.xml.dom.DOMCategory.text;
@@ -12,7 +13,7 @@ import static io.qameta.allure.Allure.step;
 public class SearchTests extends TestBase {
 
 
-   /* @Test
+    @Test
     void successfulSearchTest() {
 
         step("Type search", () -> {
@@ -26,54 +27,24 @@ public class SearchTests extends TestBase {
 
 
 
-    }*/
+    }
 
     @Test
-    void successfulSearchTest2() {
+    void unSuccessfulSearchTest2() {
 
         step("Type search", () -> {
             $(accessibilityId("Search Wikipedia")).click();
             $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
         });
 
-
-        //Appius Claudius Caecus
-        //Expand table
-
-
-        //$(id("org.wikipedia.alpha:id/page_list_item_title")).click();
-
-        //$(byText("Appius Claudius Caecus")).click();
-        //$(accessibilityId("Appius Claudius Caecus")).click();
-        //$(accessibilityId("Appium")).click();
-        sleep(10000);
-
         $$(id("org.wikipedia.alpha:id/page_list_item_title")).get(0).click();
 
 
-
-        //android.view.View
-         //       Appium is an open
-
-
-
-        /*
-
-        package	org.wikipedia.alpha
-        class	android.widget.TextView
-        text	Appius Claudius Caecus
-        resource-id	org.wikipedia.alpha:id/page_list_item_title
-
-         */
+        step("Verify content", () -> {
+            $(accessibilityId("An error occurred")).shouldBe(exist);
+        });
 
 
-
-/*
-        step("Verify content found", () ->
-                $$(id("org.wikipedia.alpha:id/page_list_item_title"))
-                        .shouldHave(sizeGreaterThan(0)));
-
- */
     }
 
 
